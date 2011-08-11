94363c1f9dbc2b0d27e3acfe8d74262439de826e
Jeff Jolma <jeff@animoto.com>
2011-08-11 10:24:09 -0400

    turn off scroll bars

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+;; You really don't need these; trust me.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 ;; 
 ;; Load paths
 ;;
@@ -58,6 +64,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.otl$" . outline-mode))
 
+
+;; disable verticle scroll bar
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; TODO try
 ;; ;; Turn off mouse interface early in startup to avoid momentary display
