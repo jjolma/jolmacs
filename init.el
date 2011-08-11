ed696edafe2a101b19fb9e0390433ee2def33893
Jeff Jolma <jeff@animoto.com>
2011-08-11 11:45:49 -0400

    remove stale TODO

--- init.el
+++ init.el
@@ -70,12 +70,3 @@
   (set-frame-position (selected-frame) 0 0)
   (set-frame-size (selected-frame) 1000 1000))
 (maximize-frame)
-
-;; TODO try
-;; ;; Turn off mouse interface early in startup to avoid momentary display
-;; ;; You really don't need these; trust me.
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-;; (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-
